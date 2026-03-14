91d8d1d487f65443b43dc7dc9c55c4cdf543dee3
erginahmetc-sudo <ergin.ahmetc@gmail.com>
2026-03-14 17:19:00 +0300

    satışlar açılmıyordu

--- frontend/src/pages/SalesPage.jsx
+++ frontend/src/pages/SalesPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { salesAPI, productsAPI, settingsAPI, customersAPI, logsAPI } from '../services/api';
 import { birFaturaAPI } from '../services/birFaturaService';
 
